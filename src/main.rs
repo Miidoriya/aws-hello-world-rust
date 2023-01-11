74470e5743d81ebba3b6d45f8e76bb9311a48d9d
Miidoriya <99621562+Miidoriya@users.noreply.github.com>
2023-01-11 20:34:06 +0000

    [main] lists DynamoDB tables within AWS using credentials from the environment the code is run in

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,22 @@ use aws_sdk_dynamodb::{Client, Error};
 
 /// Lists your DynamoDB Tables in the default Region or us-east-1 if a default Region isn't set.
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<(), Error> {
+    let region_provider = RegionProviderChain::default_provider().or_else("us-east-1");
+    let config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&config);
+
+    let resp = client.list_tables().send().await?;
+
+    println!("Tables:");
+
+    let names = resp.table_names().unwrap_or_default();
+    for name in names {
+        println!("  {}", name);
+    }
+
+    println!();
+    println!("Found {} tables.", names.len());
+
+    Ok(())
 }
